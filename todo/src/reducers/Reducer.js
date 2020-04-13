21cedcb1120cf9eb2d870c5e96e3659bdb32a2a9
Latosha Tims <ltims080913@hotmail.com>
2020-04-13 15:10:15 -0700

    set up reducers folder, created reducer.js file, created and exported both the initialState and basic reducer function

---
+++ todo/src/reducers/Reducer.js
@@ -0,0 +1,14 @@
+import React, { useState, useReducer } from 'react';
+
+
+export const initialState = {
+   item: 'Learn about reducers',
+   completed: false, 
+   id: Date.now()
+
+}
+export const reducer = (state, action) => {
+   
+    return state
+}
+
