9587b3019b6d89e4246d8e103bb865b36fae3b7c
Latosha Tims <ltims080913@hotmail.com>
2020-04-14 08:27:10 -0700

    styled h1 title with styled components

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,15 +1,32 @@
 import React from 'react';
 import './App.css';
 import TodoForm from './components/TodoForm';
+import Background from './images/white-book-page-on-yellow-surface-1296726.jpg'
+import styled from 'styled-components'
 
+const ImageBackground = styled.div `
+display: flex;
+flex-direction: column;
+justify-content: center;
+background-image: url(${Background});
+background-size: cover;
+height: 100vh;
+`
+const Title = styled.h1 `
+padding: 2%;
+margin: -15% 35% 5% 35%;
+color: slategray;
+font-size: 2.5rem;
 
+`
 
 function App() {
   return (
-    <div className="App">
+    <ImageBackground>
+      <Title>Keep Calm And...<br/> To-Do List</Title>
       <TodoForm/>
       {/* <Todo/> */}
-    </div>
+    </ImageBackground>
   );
 }
 
